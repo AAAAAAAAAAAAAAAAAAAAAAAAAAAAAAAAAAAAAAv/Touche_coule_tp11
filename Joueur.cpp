122ce22ec2dd54944670f1db5ae68abf659da6af
Guillaume-ctrl <ps3aknew@gmail.com>
2024-05-29 17:27:23 +0200

    Modif Joueur

--- Joueur.cpp
+++ Joueur.cpp
@@ -2,5 +2,12 @@
 
 Joueur::Joueur(std::string nom) : nomJoueur(nom)
 {
+    this->ControlleurJoueur();
+    // on lui crée son propre plateau
+    this->pPlateau = new Plateau;
+}
 
+~Joueur::Joueur()
+{
+    delete pPlateau;
 }
